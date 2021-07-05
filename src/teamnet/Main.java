cae3ee15dff986b9d5faf212f794e0855df160a4
Theodor Avrigeanu <theodor.avrigeanu@teamnet.ro>
2021-07-06 01:27:40 +0300

    Am optimizat optiunile prezente

--- src/teamnet/Main.java
+++ src/teamnet/Main.java
@@ -3,13 +3,13 @@ package teamnet;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Aplicatie finala\n");
+        System.out.println("Feature 2\n");
 
-        System.out.println("optiune 1");
-        System.out.println("optiune 2");
-        System.out.println("optiune 3");
-        System.out.println("optiune 4");
-        System.out.println("optiune 5");
+        System.out.println("Optiune 1");
+        System.out.println("Optiune 2");
+        System.out.println("Optiune 3");
+        System.out.println("Optiune 4");
+        System.out.println("Optiune 5");
 
     }
 }
