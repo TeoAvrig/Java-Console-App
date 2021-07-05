89aff7ca6b3ce03f5ca406a1da46ab072846a726
Theodor Avrigeanu <theodor.avrigeanu@teamnet.ro>
2021-07-06 01:20:53 +0300

    Am adaugat primele 5 optiuni

--- src/teamnet/Main.java
+++ src/teamnet/Main.java
@@ -3,6 +3,13 @@ package teamnet;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Initializare!");
+        System.out.println("Feature 1\n");
+
+        System.out.println("optiune 1");
+        System.out.println("optiune 2");
+        System.out.println("optiune 3");
+        System.out.println("optiune 4");
+        System.out.println("optiune 5");
+
     }
 }
